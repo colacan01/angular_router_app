643a0184b26b53911bf14f3ba963bb043eed03f0
고진호 <colacan01@gmail.com>
2023-12-05 09:46:17 +0900

    목록에서 판매금액 표시 수정중

--- src/app/goods-list/goods-card/goods-card.component.ts
+++ src/app/goods-list/goods-card/goods-card.component.ts
@@ -9,19 +9,4 @@ import { Good }                                 from '../../interface_category';
 })
 export class GoodsCardComponent {
   @Input() good!: Good;
-
-  // hasPrice: number = 0;
-  // _Base_Price?: number | undefined;
-  // _Sale_Price?: number | undefined;
-  // _Discount_Per?: number | undefined;
-
-  // ngOnInit() {
-  //   if (this.good.price_histories?.length != 0) {
-  //     this.hasPrice = 1
-  //     this._Base_Price = this.good.price_histories[0].base_price;
-  //     this._Sale_Price = this.good.price_histories[0].sale_price;
-  //     this._Discount_Per = this.good.price_histories[0].discount_per;
-  //   }
-
-  // }  
 }
