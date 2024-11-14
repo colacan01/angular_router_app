710e7033f4042daa074c931d60af80e7e74bd3f8
jinho.Ko <colacan01@gmail.com>
2024-11-14 21:10:16 +0900

    .

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
         // baseURL: 'http://172.30.1.55:48080/api/',
-        baseURL: 'http://172.30.1.112:5153/api/',
+        // baseURL: 'http://172.30.1.112:5153/api/',
+        baseURL: 'http://localhsot:5153/api/',
 };
